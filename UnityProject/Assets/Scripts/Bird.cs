402b52409f780cd33e8017261b7990ade3ff1072
dechi1220 <57282811+dechi1220@users.noreply.github.com>
2019-11-23 16:22:50 +0800

    1123_04:22

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -5,15 +5,39 @@ public class Bird : MonoBehaviour
     //跳越高度，是否死亡
     [Header("跳越高度"),Range(10,2000)]
     public int jump = 100;
+    [Header("旋轉角度"), Range(0, 100)]
+    public float angle = 10;
+
     [Header("死了沒"), Tooltip("ture 死亡, false 還沒死")]
     public bool dead ;
+    [Header("剛體")]
+    public Rigidbody2D r2d;
+
+    public GameObject goScore, goGM;
+
 
     /// <summary>
     /// 定義小雞跳躍
     /// </summary>
-    private void Jump()
+    private void Jump( )
     {
-    
+        //如果 玩家 按下 左鍵
+        // 輸入. 按下左鍵(案件列舉.滑鼠左鍵)(手機觸控)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("玩家按下左鍵");
+            r2d.Sleep();                                                      //小雞剛體.睡著( ) - 重設剛體所有資訊
+            r2d.gravityScale = 2;                                   //小雞剛體.重力 = 1;   //小雞 往上跳
+            r2d.AddForce(new Vector2(0, jump));  //小雞剛體.增加推力(二為向量(左右，上下));
+
+            goScore.SetActive(true);                          // 分數顯示
+            goGM.SetActive(true);                             // GM 顯示
+        }
+
+        // Rigidbody2D.SetRotation(float)設定角度(角度)
+        // Rigidbody2D.velocity 加速度(二為向量 x,y)
+        r2d.SetRotation(angle * r2d.velocity.y);
+
     }
 
     /// <summary>
@@ -21,6 +45,14 @@ public class Bird : MonoBehaviour
     /// </summary>
     private void Dead()
     {
+
+    }
+    
+    //固定幀數 0.002 一幀：要控制物理請寫在此事件內
+    private void FixedUpdate()
+
+    {
+        Jump( );
 
     }
 
